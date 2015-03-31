a2667f4b493c6bec17d9b2ce33898c577edf7c7e
Constantin <botnary@gmail.com>
2015-03-30 20:37:36 -0400

    Added helper

--- src/Fusion/Rewrite/functions.php
+++ src/Fusion/Rewrite/functions.php
@@ -7,14 +7,17 @@
  */
 namespace Fusion\Rewrite;
 
-function link($tpl = null, $lang = null, $idc = null, $id = null, $categoryTitle = null, $pageTitle = null)
+class Helper
 {
-    $url = new UrlBuilder();
-    $url->setTpl($tpl);
-    $url->setLang($lang);
-    $url->setIdc($idc);
-    $url->setIdc($id);
-    $url->setCategoryTitle($categoryTitle);
-    $url->setPageTitle($pageTitle);
-    return $url->build();
+    function link($tpl = null, $lang = null, $idc = null, $id = null, $categoryTitle = null, $pageTitle = null)
+    {
+        $url = new UrlBuilder();
+        $url->setTpl($tpl);
+        $url->setLang($lang);
+        $url->setIdc($idc);
+        $url->setIdc($id);
+        $url->setCategoryTitle($categoryTitle);
+        $url->setPageTitle($pageTitle);
+        return $url->build();
+    }
 }
